1855422673edf0c5272eacd7d6f3070c442070e7
Madhav <madhavv8528@gmail.com>
2025-05-31 19:52:22 +0530

    .

--- 27goMutex/main.go
+++ 27goMutex/main.go
@@ -5,35 +5,33 @@ import (
 	"sync"
 )
 
-var jojo []int
-
 func main() {
 
 	wg := &sync.WaitGroup{}
 	mut := &sync.Mutex{}
+	var jojo []int
 	wg.Add(3)
-	func(wg *sync.WaitGroup, mut *sync.Mutex) {
+	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
 		fmt.Println("One func")
 		mut.Lock()
 		jojo = append(jojo, 1)
 		mut.Unlock()
 		wg.Done()
 	}(wg, mut)
-	func(wg *sync.WaitGroup, mut *sync.Mutex) {
+	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
 		fmt.Println("Two func")
 		mut.Lock()
 		jojo = append(jojo, 2)
 		mut.Unlock()
 		wg.Done()
 	}(wg, mut)
-	func(wg *sync.WaitGroup, mut *sync.Mutex) {
+	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
 		fmt.Println("Third func")
 		mut.Lock()
 		jojo = append(jojo, 3)
 		mut.Unlock()
 		wg.Done()
 	}(wg, mut)
-
-	fmt.Println(jojo)
 	wg.Wait()
+	fmt.Println(jojo)
 }
